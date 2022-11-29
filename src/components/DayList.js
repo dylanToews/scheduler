2b17d1f707fc56b26c155d6d2c3c7a94d9fd7f16
dylanToews <dylan.toews@yahoo.ca>
2022-11-29 13:30:27 -0700

    set up conditionals in DayListItem to render different text based on number of spots available

---
+++ src/components/DayList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import DayListItem from "components/DayListItem";
+
+
+
+export default function DayList(props) {
+  const days = props.days;
+  const listDays = 
+  return (
+    <ul>
+
+
+    </ul>
+
+  );
+}
